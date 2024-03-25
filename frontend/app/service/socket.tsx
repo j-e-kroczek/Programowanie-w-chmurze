65b3f4d576fdac46b4da9812d86a23fb2b6d745b
j-e-kroczek <108948482+j-e-kroczek@users.noreply.github.com>
2024-03-25 11:51:17 +0100

    Added-Lista-1-dir

--- frontend/app/service/socket.tsx
+++
@@ -1,8 +0,0 @@
-import io from "socket.io-client";
-
-if (process.env.SOCKET_URL === undefined) {
-  throw new Error("SOCKET_URL is not defined");
-}
-export const socket = io(process.env.SOCKET_URL, {
-  autoConnect: false
-});
--- frontend/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        API_URL: process.env.API_URL || 'http://localhost:3000/api',
-        SOCKET_URL: process.env.SOCKET_URL || 'http://localhost:3000',
-    },
-};
-
-export default nextConfig;
